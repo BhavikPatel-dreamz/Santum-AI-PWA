e9e02b1c907d08aa3183a64081f3c57a64f177fa
VatsalRangoonwala <info@dynamicdreamz.com>
2026-04-29 12:55:02 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ const OnboardingSlide = dynamic(() => import("../components/onboarding/Onboardin
 
 const slides = [
   {
-    image: "/icons/Artboard 2@2x-100.jpg",
+    image: "/icons/artboard-2.jpg",
     title: "Welcome to Amigo, Great Friend to Chat",
     desc: "Proin molestie pulvinar vitae enim erat morbi eu. Malesuada eros nisi augue.",
     btnLabel: "Next",
   },
   {
-    image: "/icons/Artboard 2@2x-100.jpg",
+    image: "/icons/artboard-2.jpg",
     title: "The Intelligent Way to Get Started",
     desc: "Quisque blandit risus duis odio. In pretium nibh velit a aenean vitae porta euismod.",
     btnLabel: "Next",
   },
   {
-    image: "/icons/Artboard 2@2x-100.jpg",
+    image: "/icons/artboard-2.jpg",
     title: "Accelerate Your Learning with Amigo",
     desc: "Pulvinar in et eu volutpat mauris viverra ut orci. Lacus placerat volutpat pharetra a.",
     btnLabel: "Get Started",
