d48a4bca237355078ef0335ecda819090039c859
VatsalRangoonwala <vatsal.rangoonwala926@gmail.com>
2026-05-14 17:08:56 +0530

    refactor: remove purpose attributes from manifest icons for simplification

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -14,13 +14,11 @@ export default function manifest() {
         src: "/favicon/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable"
       },
       {
         src: "/favicon/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any"
       },
     ],
   };
